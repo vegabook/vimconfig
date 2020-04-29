cdd7a7a8ba784addc3b761904d568d535091ab99
vegabook <thomas@crvm.io>
2020-04-29 16:47:18 +0100

    with auto variable insertion bashrc

--- installplug.sh
+++ installplug.sh
@@ -18,12 +18,25 @@ ln -s $HOME/vimconfig/nvimrc.vim $HOME/.config/nvim/init.vim
 ln -s $HOME/vimconfig/colors $HOME/.config/nvim/colors
 ln -s $HOME/vimconfig/tmux.conf $HOME/.tmux.conf
 
+# install tmux status line colour variables
+# in bashrc if they do not exist
+if grep -Fxq "TMUXSTATBG" $HOME/.bashrc
+then
+	:
+else
+	echo "" >> $HOME/.bashrc
+	echo '# environment variables for tmux colours' >> $HOME/.bashrc
+	echo 'export $TMUXSTATBG="#6655CC"' >> $HOME/.bashrc
+	echo 'export $TMUXSTATFG="#EEEEEE"' >> $HOME/.bashrc
+fi
+
 # instructions to user
 echo "---------------------------------------"
 echo "Now got into (neo)vim and :PlugInstall"
+echo ""
 echo "environment variables $TMUXSTATFG and"
 echo "$TMUXSTATBG can be used for Tmux "
-echo "status line colours. They go in .bashrc"
+echo "status line colours. They're in .basrhc"
 
 
 
