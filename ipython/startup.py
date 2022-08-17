5a1d9676357a470ebf23b4d7ea8c84d9b4256775
vegabook <thomas@crvm.io>
2022-08-17 12:04:58 +0100

    more rows for pandas

--- ipython/startup.py
+++ ipython/startup.py
@@ -4,4 +4,7 @@ import datetime as dt
 import os
 import pyarrow as pa
 import pyarrow.parquet as pq
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
 
